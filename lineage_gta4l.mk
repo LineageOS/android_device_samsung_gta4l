cbf3e743bbd8447970f6d49ba619212ac5c8a564
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    gta4l: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_gta4l.mk
+++ lineage_gta4l.mk
@@ -37,7 +37,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=gta4lxx \
-    PRIVATE_BUILD_DESC="gta4lxx-user 12 SP1A.210812.016 T505XXS7CXB1 release-keys"
-
-BUILD_FINGERPRINT := "samsung/gta4lxx/qssi:12/SP1A.210812.016/T505XXS7CXB1:user/release-keys"
+    BuildDesc="gta4lxx-user 12 SP1A.210812.016 T505XXS7CXB1 release-keys" \
+    BuildFingerprint="samsung/gta4lxx/qssi:12/SP1A.210812.016/T505XXS7CXB1:user/release-keys"
+    DeviceProduct=gta4lxx \
+    SystemName=gta4lxx
